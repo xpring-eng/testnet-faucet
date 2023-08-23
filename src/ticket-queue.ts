8bfa712c355bc7324c9cbfcded15c82124c1a04e
JST5000 <aim4math@gmail.com>
2023-08-23 14:42:11 -0700

    Stringify

--- src/ticket-queue.ts
+++ src/ticket-queue.ts
@@ -84,7 +84,7 @@ export async function getTicket(client: Client) {
       })
       .catch((error) => {
         console.log(
-          `Failed to create tickets. Error: ${error}`
+          `Failed to create tickets. Error: ${JSON.stringify(error)}`
         );
       });
   }
